11c5a30a44de600ecb05e3ddd73feb28ce3f0eae
Vincent Abesamis <vincentabesamis03@gmail.com>
2021-10-07 14:27:51 +0800

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+
+const Home: NextPage = () => {
+  return (
+    <>
+    <Head>
+      <title>Let Leni Lead</title>
+    </Head>
+    <div className="w-full h-screen bg-pink-400 flex justify-center items-center">
+      <h1 className="text-5xl text-white">Let Leni Lead</h1>
+    </div>
+    </>
+  )
+}
+
+export default Home
