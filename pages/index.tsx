39f1326ab445a38b7521eec4b1eb8e2aac829e40
Vincent Abesamis <vincentabesamis03@gmail.com>
2021-10-07 15:31:48 +0800

    Adjust styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
     <Head>
       <title>🇵🇭 Let Leni Lead</title>
     </Head>
-    <div className="w-full h-screen bg-gray-900 flex flex-col justify-center items-center">
+    <div className="w-full h-screen bg-black flex flex-col justify-center items-center">
       {/* <h1 className="text-5xl text-white">🇵🇭 Let Leni Lead 🇵🇭</h1>
       <div className="text-center mt-4">
         <h2 className="text-4xl text-white"><a href="https://www.facebook.com/hashtag/LabanLeni2022">#LetLeniLead</a></h2>
@@ -16,8 +16,9 @@ const Home: NextPage = () => {
       </div> */}
       <div className="relative group">
         <div className="absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-full blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
-        <div className="relative rounded-full h-12 w-12 bg-pink-500 blur opacity-90"></div>
+        <div className="relative rounded-full h-8 w-8 bg-pink-500 blur opacity-90"></div>
       </div>
+      #LabanLeni2022
     </div>
     </>
   )
